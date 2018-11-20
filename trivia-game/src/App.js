5dad7676c2115806f96d40e7c45f243dc41be002
Anis Merchant <anisxmerchant@gmail.com>
2018-11-20 11:05:29 -0500

    project nine: Create navbar component

--- trivia-game/src/App.js
+++ trivia-game/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
+// import './App.css';
+import './css/style.css';
 import Home from './components/Home';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
 
       <div className="App">
+        <NavBar />
         <Home />
       </div>
 
---
+++ trivia-game/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div className="navigation">
+                <img className = "navigation--image" src="./assets/images/nav-background.png" alt="Navigation Background Cover" />
+            </div>
+        )
+    }
+}
+
+export default NavBar;
